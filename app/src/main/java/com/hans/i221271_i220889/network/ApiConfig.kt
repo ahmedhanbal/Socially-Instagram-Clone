d69ab5f784f31213ac79286093042f7cf6a9f383
ahmedhanbal <ahmed.alizahid14@gmail.com>
2025-11-20 23:24:00 +0500

    before submission

--- app/src/main/java/com/hans/i221271_i220889/network/ApiConfig.kt
+++ app/src/main/java/com/hans/i221271_i220889/network/ApiConfig.kt
@@ -1,7 +1,7 @@
 package com.hans.i221271_i220889.network
 
 object ApiConfig {
-    const val BASE_URL = "http://172.16.49.52/socially_api/"
+    const val BASE_URL = "http://172.17.50.43/socially_api/"
     
     const val UPLOADS_BASE_URL = "${BASE_URL}uploads/"
 
