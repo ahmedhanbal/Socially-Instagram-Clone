052d6958e1bd457505268f7ba53c5b8a3fe7300a
naveedahmed5 <naveedahmedbajwa2@gmail.com>
2025-11-20 16:07:00 +0500

    Update API base URL to use PC IP address 192.168.100.8

--- app/src/main/java/com/hans/i221271_i220889/network/ApiConfig.kt
+++ app/src/main/java/com/hans/i221271_i220889/network/ApiConfig.kt
@@ -1,10 +1,10 @@
-hpackage com.hans.i221271_i220889.network
+package com.hans.i221271_i220889.network
 
 object ApiConfig {
     // Change this to your actual server IP when testing
     // For local XAMPP: http://10.0.2.2/socially_api/ (Android Emulator)
     // For physical device: http://YOUR_PC_IP/socially_api/
-    const val BASE_URL = "http://127.0.0.1/socially_api/"
+    const val BASE_URL = "http://192.168.100.8/socially_api/"
     
     const val UPLOADS_BASE_URL = "${BASE_URL}uploads/"
     
